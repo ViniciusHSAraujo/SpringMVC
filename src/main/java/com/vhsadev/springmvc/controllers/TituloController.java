ff0caf6f5098d65609e71d88db15936b8f869b15
Vinícius Araújo <44041869+ViniciusHSAraujo@users.noreply.github.com>
2020-03-17 13:43:04 -0300

    Commit inicial

---
+++ src/main/java/com/vhsadev/springmvc/controllers/TituloController.java
@@ -0,0 +1,10 @@
+package com.vhsadev.springmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TituloController {
+	public String Novo() {
+		return "CadastroTitulo";
+	}
+}
